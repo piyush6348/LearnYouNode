f14872e183861db507941aa0520f6358634ceb93
piyush6348 <piyush6348@gmail.com>
2018-01-30 11:27:48 +0530

    routers added

--- nodeLectureLearning/routers/route/students.js
+++ nodeLectureLearning/routers/route/students.js
@@ -11,10 +11,17 @@ route.get('/',(req,res,next)=>{
     res.send(students);
 })
 
-route.get('/:id',(req,res) => {
+// There is no filtering on this path
+// particularly as /abc,/1
+// both will result into execution of response handler
+// of this request.
+route.get('/:id',(req,res,next) => {
+    if(isNaN(parseInt(req.params.id)))
+        next();
     res.send(students[req.params.id - 1]);
 })
 
+// req.baseUrl gives the parent path available to current file
 route.post('/new',(req,res)=>{
     console.log("Post method called for "+req.body.name);
 
@@ -22,6 +29,7 @@ route.post('/new',(req,res)=>{
         name: req.body.name,
         age: req.body.age
     })
-    res.send("Student added");
+    console.log("baseUrl "+ req.baseUrl);
+    res.redirect(req.baseUrl);
 })
 module.exports=route;
